58ffcc7a9a20c34081bc89d9d428002771b18a52
Ratnadeep Bhattacharjee <ratnadeep18@gmail.com>
2017-12-06 00:13:09 +0530

    Revised the two files.

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,10 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+
+def tim():
+        Print("Tim says hello")
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
